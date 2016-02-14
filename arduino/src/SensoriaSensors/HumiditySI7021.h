1576fcbce9413ed29280e18902a441279e071f6c
SukkoPera <software@sukkology.net>
2016-02-14 20:01:54 +0100

    Add support for the SI7021 temperature/humidity sensor

---
+++ arduino/src/SensoriaSensors/HumiditySI7021.h
@@ -0,0 +1,44 @@
+#include <SensoriaInternals/Sensor.h>
+
+#include <SI7021.h>
+
+class SI7021HumiditySensor: public Sensor {
+private:
+  SI7021 sensor;
+
+public:
+	SI7021HumiditySensor () {
+	}
+
+	bool begin (FlashString name, FlashString description) {
+		if (Sensor::begin (name, description, F("20160214"))) {
+      if (!sensor.begin ()) {
+        DPRINTLN ("SI7021.begin() failed");
+        return false;
+      } else {
+        return true;
+      }
+		} else {
+			return false;
+		}
+	}
+
+
+	char *read (char *buf, const byte size _UNUSED) {
+    si7021_env reading = sensor.getHumidityAndTemperature();
+  	float h = reading.humidityBasisPoints / 100.0;
+		float t = reading.celsiusHundredths / 100.0;
+		if (!isnan (h) && !isnan(t)) {
+			buf[0] = 'T';
+			buf[1] = ':';
+			floatToString (t, buf + 2);
+			int l = strlen (buf);
+			buf[l] = ' ';
+			buf[l + 1] = 'H';
+			buf[l + 2] = ':';
+			floatToString (h, buf + l + 3);
+		}
+
+		return buf;
+	}
+};
