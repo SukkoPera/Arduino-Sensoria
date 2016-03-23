2c14e12463117e071f1065e7fe8b12c42b4d7fce
SukkoPera <software@sukkology.net>
2016-03-23 23:58:57 +0100

    Improved logger

--- python/logger.py
+++ python/logger.py
@@ -7,6 +7,8 @@ import argparse
 
 import Sensoria
 
+DEFAULT_INTERVAL = 5		# minutes
+
 parser = argparse.ArgumentParser (description = 'Plot some data')
 parser.add_argument ('addresses', metavar = "ADDRESS", nargs = '*',
                      help = "Address of node to query")
@@ -14,6 +16,8 @@ parser.add_argument ('--no-autodiscover', "-n", action = 'store_true', default =
                      help = "Do not autodiscover nodes")
 parser.add_argument ('--read-actuators', "-a", action = 'store_true', default = False,
                      help = "Read Actuators too")
+parser.add_argument ('--interval', "-i", default = DEFAULT_INTERVAL, type = int,
+                     help = "Interval between reads (minutes)")
 
 args = parser.parse_args ()
 sensoria = Sensoria.Client (servers = args.addresses, autodiscover = not args.no_autodiscover)
@@ -23,20 +27,26 @@ if len (sensoria.transducers) == 0:
 
 db = Sensoria.DB ()
 
+# FIXME: Stop with SIGHUP
 while True:
 	try:
 		data = {}
 		now = datetime.datetime.now ()
-		for name, sensor in sensoria.sensors.iteritems ():
-			try:
-				data[name] = sensor.read (raw = True)
-				print "%s -> %s" % (name, data[name])
-			except Exception as ex:
-				print "Cannot read sensor %s: %s" % (name, str (ex))
+		for tname in sorted (sensoria.transducers.iterkeys ()):
+			t = sensoria.transducers[tname]
+			if t.genre == Sensoria.SENSOR or (t.genre == Sensoria.ACTUATOR and args.read_actuators):
+				try:
+					data[tname] = t.read (raw = True)
+					print "%s -> %s" % (tname, data[tname])
+				except Exception as ex:
+					print "Cannot read sensor %s: %s" % (tname, str (ex))
+
+		# All readings done, save to DB
 		db.insert (now, data, commit = True)
 		print "."
 	except Exception as ex:
 		print "ERROR: %s" % str (ex)
 		raise
 
-	time.sleep (5 * 60)
+	# Sleep until next update
+	time.sleep (args.interval * 60)
