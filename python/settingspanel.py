b41e700328b260386f814a65c50fa67047336fe9
SukkoPera <software@sukkology.net>
2018-01-15 23:11:04 +0100

    Add missing file

---
+++ python/settingspanel.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import copy
+
+import wx
+import wx.grid
+
+import Sensoria
+from Sensoria.stereotypes.ValueSetData import ValueSetData
+
+class SettingsEditDialog (wx.Dialog):
+	NSETTINGS = ValueSetData.NVALUES
+
+	def __init__(self, t):
+		super (SettingsEditDialog, self).__init__ (None, title = "Edit Settings for %s" % t.name, style = wx.DEFAULT_DIALOG_STYLE | wx.THICK_FRAME | wx.TAB_TRAVERSAL)
+
+		self.transducer = t
+
+		sizer = wx.BoxSizer (wx.VERTICAL)
+
+		self._textCtrls = []
+		gs = wx.FlexGridSizer (SettingsEditDialog.NSETTINGS, 2, 10, 5)
+		for i in xrange (0, SettingsEditDialog.NSETTINGS):
+			if i < len (t.lastRead.values) and t.lastRead.values[i]:
+				s = str (t.lastRead.values[i])
+			else:
+				s = ""
+
+			tc = wx.TextCtrl (self, -1, s)
+			self._textCtrls.append (tc)
+
+			gs.Add (wx.StaticText (self, -1, 'Value %u:' % i), 0, wx.ALIGN_CENTER_VERTICAL)
+			gs.Add (tc, 1, wx.EXPAND)
+
+		gs.AddGrowableCol (1, 1)
+		sizer.Add (gs, 0, wx.EXPAND | wx.ALL, 30)
+
+		btnBox = wx.BoxSizer (wx.HORIZONTAL)
+		btnCancel = wx.Button (self, wx.ID_CANCEL, '&Cancel')		# Using ID_CANCEL automatically closes dialog
+		btnBox.Add (btnCancel, 0)
+		btnOk = wx.Button (self, wx.ID_OK, '&OK')
+		btnBox.Add (btnOk, 0)
+		btnOk.SetDefault ()
+		btnOk.Bind (wx.EVT_BUTTON, self.onOk)
+		sizer.Add (btnBox, 0, wx.ALIGN_RIGHT | wx.RIGHT | wx.BOTTOM, 10)
+
+		w = wx.DisplaySize ()[0] / 3
+		self.SetSizer (sizer)
+		self.SetMinSize ((w, 200))
+		self.Fit ()
+		sz = self.GetSize ()
+		self.SetSizeHints (minW = sz.GetWidth (), minH = sz.GetHeight ())
+
+		self.CentreOnParent (wx.BOTH)
+		self.SetFocus ()
+
+	def onOk (self, event):
+		print "Saving Settings"
+		vs = ValueSetData ()
+		for i, tc in enumerate (self._textCtrls):
+			v = tc.GetValue ()
+			# FIXME: How to tell if this should be None or an empty string?
+			if len (v) > 0:
+				vs.values[i] = v
+
+		try:
+			print self.transducer.write (vs)
+		except Sensoria.Error as ex:
+			wx.MessageBox ("Unable to save settings.\n\n%s" % str (ex), "Error", wx.ICON_ERROR)
+		self.Close ()
+		event.Skip ()
