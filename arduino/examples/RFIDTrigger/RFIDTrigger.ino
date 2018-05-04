bae6db711f22eaa5e88ed6542a08bb80a31996e1
SukkoPera <software@sukkology.net>
2018-05-04 22:30:30 +0200

    Fix indentation

--- arduino/examples/RFIDTrigger/RFIDTrigger.ino
+++ arduino/examples/RFIDTrigger/RFIDTrigger.ino
@@ -40,62 +40,62 @@ boolean relayState;
 #define LED_PIN LED_BUILTIN
 
 void mypanic (int interval) {
-  pinMode (LED_PIN, OUTPUT);
-  while (42) {
-    digitalWrite (LED_PIN, HIGH);
-    delay (interval);
-    digitalWrite (LED_PIN, LOW);
-    delay (interval);
-  }
+	pinMode (LED_PIN, OUTPUT);
+	while (42) {
+		digitalWrite (LED_PIN, HIGH);
+		delay (interval);
+		digitalWrite (LED_PIN, LOW);
+		delay (interval);
+	}
 }
 
 void setup () {
-  Serial.begin (9600);
+	Serial.begin (9600);
 
-  //~ Serial3.begin (9600);
-  swSerial.begin (9600);
+	//~ Serial3.begin (9600);
+	swSerial.begin (9600);
 	if (!comm.begin (swSerial, WIFI_SSID, WIFI_PASSWORD)) {
 		Serial.println (F("WiFi error"));
 		mypanic (100);
 	}
 
-  client.begin (comm);
+	client.begin (comm);
 	while (!client.registerNode (relayNodeAddr)) {
 		Serial.println (F("Cannot register Relay Node"));
-    delay (1000);
+		delay (1000);
 	}
-  Serial.println (F("Relay Node registered"));
+	Serial.println (F("Relay Node registered"));
 
 	relayActuator = client.getActuator (RELAY_ACTNAME);
 	if (!relayActuator) {
 		mypanic (2000);
 	}
-  Stereotype *st;
-  if (!relayActuator -> read (st)) {
+	Stereotype *st;
+	if (!relayActuator -> read (st)) {
 		Serial.println (F("Relay state read failed"));
 	} else {
 		RelayData &data = *static_cast<RelayData *> (st);
-    relayState = data.state == RelayData::STATE_ON;
-    Serial.print (F("Initial relay state = "));
-    Serial.println (relayState ? F("ON") : F("OFF"));
+		relayState = data.state == RelayData::STATE_ON;
+		Serial.print (F("Initial relay state = "));
+		Serial.println (relayState ? F("ON") : F("OFF"));
 	}
 
-  // Init RFID reader
-  SPI.begin ();			// Init SPI bus
+	// Init RFID reader
+	SPI.begin ();			// Init SPI bus
 	mfrc522.PCD_Init ();		// Init MFRC522
 	mfrc522.PCD_DumpVersionToSerial ();
-  Serial.println (F("RFID Reader inited"));
+	Serial.println (F("RFID Reader inited"));
 }
 
 void loop () {
-  static MFRC522::Uid lastUid = {0, {0}, 0};
-  static unsigned long lastCardDetected = 0;
+	static MFRC522::Uid lastUid = {0, {0}, 0};
+	static unsigned long lastCardDetected = 0;
 
 	// Look for new cards
 	if (!mfrc522.PICC_IsNewCardPresent ()) {
-    if (millis () - lastCardDetected >= 1000) {
-      lastUid.size = 0;     // Enought to consider the next card new
-    }
+		if (millis () - lastCardDetected >= 1000) {
+			lastUid.size = 0;     // Enough to consider the next card new
+		}
 		return;
 	}
 
@@ -107,55 +107,55 @@ void loop () {
 	// Dump debug info about the card; PICC_HaltA() is automatically called
 	//mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
-  if (mfrc522.uid.size == KEY_SIZE) {
-    if (!compareUid (lastUid, mfrc522.uid)) {
-      // New key presented!
-      if (compareUid (mfrc522.uid, GOOD_KEY, KEY_SIZE)) {
-        Serial.println ("GOOD!");
-        toggleRelay ();
-      } else {
-        Serial.println ("Bad!");
-      }
+	if (mfrc522.uid.size == KEY_SIZE) {
+		if (!compareUid (lastUid, mfrc522.uid)) {
+			// New key presented!
+			if (compareUid (mfrc522.uid, GOOD_KEY, KEY_SIZE)) {
+				Serial.println ("GOOD!");
+				toggleRelay ();
+			} else {
+				Serial.println ("Bad!");
+			}
 
-      lastUid = mfrc522.uid;
-    }
+			lastUid = mfrc522.uid;
+		}
 
-    lastCardDetected = millis ();
-  }
+		lastCardDetected = millis ();
+	}
 }
 
 
 void toggleRelay () {
-  // Toggle remote relay
-  RelayData rd;
-  if (relayState) {
-    rd.state = RelayData::STATE_OFF;
-  } else {
-    rd.state = RelayData::STATE_ON;
-  }
-  relayActuator -> write (rd);
-  relayState = !relayState;
-  Serial.print (F("Relay is now: "));
-  Serial.println (relayState ? F("ON") : F("OFF"));
+	// Toggle remote relay
+	RelayData rd;
+	if (relayState) {
+		rd.state = RelayData::STATE_OFF;
+	} else {
+		rd.state = RelayData::STATE_ON;
+	}
+	relayActuator -> write (rd);
+	relayState = !relayState;
+	Serial.print (F("Relay is now: "));
+	Serial.println (relayState ? F("ON") : F("OFF"));
 }
 
 
 
 boolean compareUid (byte *k1, byte *k2, byte size) {
-  boolean match = true;
-  for (int i = 0; match && i < size; i++) {
-    if (k1[i] != k2[i])
-      match = false;
-  }
+	boolean match = true;
+	for (int i = 0; match && i < size; i++) {
+		if (k1[i] != k2[i])
+			match = false;
+	}
 
-  return match;
+	return match;
 }
 
 boolean compareUid (MFRC522::Uid& k1, MFRC522::Uid& k2) {
-  return k1.size == k2.size && compareUid (k1.uidByte, k2.uidByte, k1.size);
+	return k1.size == k2.size && compareUid (k1.uidByte, k2.uidByte, k1.size);
 }
 
 boolean compareUid (MFRC522::Uid& k1, byte *k2, byte size) {
-  return k1.size == size && compareUid (k1.uidByte, k2, k1.size);
+	return k1.size == size && compareUid (k1.uidByte, k2, k1.size);
 }
 
--- arduino/examples/RelayControllerTest/RelayControllerTest.ino
+++ arduino/examples/RelayControllerTest/RelayControllerTest.ino
@@ -33,73 +33,73 @@ IPAddress outdoor1 (192, 168, 1, 154);
 #define LED_PIN LED_BUILTIN
 
 void mypanic (int interval) {
-  pinMode (LED_PIN, OUTPUT);
-  while (42) {
-    digitalWrite (LED_PIN, HIGH);
-    delay (interval);
-    digitalWrite (LED_PIN, LOW);
-    delay (interval);
-  }
+	pinMode (LED_PIN, OUTPUT);
+	while (42) {
+		digitalWrite (LED_PIN, HIGH);
+		delay (interval);
+		digitalWrite (LED_PIN, LOW);
+		delay (interval);
+	}
 }
 
 void setup () {
-  Serial.begin (9600);
+	Serial.begin (9600);
 
-  swSerial.begin (9600);
-  if (!comm.begin (swSerial, WIFI_SSID, WIFI_PASSWORD, CC_SERVER | CC_NOTIFICATIONS)) {
-    mypanic (100);
-  }
+	swSerial.begin (9600);
+	if (!comm.begin (swSerial, WIFI_SSID, WIFI_PASSWORD, CC_SERVER | CC_NOTIFICATIONS)) {
+		mypanic (100);
+	}
 
-  // Init server
-  if (!srv.begin (F("RelayControllerTest"), comm)) {
-    mypanic (500);
-  }
+	// Init server
+	if (!srv.begin (F("RelayControllerTest"), comm)) {
+		mypanic (500);
+	}
 
-  // Register relay with server
-  if (relay.begin (F("RR"), F("Controlled Relay"), 9)) {
-    if (srv.addTransducer (relay) >= 0) {
-      DPRINT (F("Actuator registered: "));
-      DPRINTLN (relay.name);
-    } else {
-      mypanic (1000);
-    }
-  } else {
-    DPRINTLN (F("Actuator failed begin()"));
-  }
+	// Register relay with server
+	if (relay.begin (F("RR"), F("Controlled Relay"), 9)) {
+		if (srv.addTransducer (relay) >= 0) {
+			DPRINT (F("Actuator registered: "));
+			DPRINTLN (relay.name);
+		} else {
+			mypanic (1000);
+		}
+	} else {
+		DPRINTLN (F("Actuator failed begin()"));
+	}
 
-  // Init client
-  client.begin (comm);
+	// Init client
+	client.begin (comm);
 
-  // Register temperature sensor
+	// Register temperature sensor
 	while (!client.registerNode (outdoor1)) {
 		Serial.println (F("Cannot register Outdoor-1 node"));
-    delay (1000);
+		delay (1000);
 	}
-  Serial.println (F("Node Outdoor-1 registered"));
+	Serial.println (F("Node Outdoor-1 registered"));
 
-  // Init Notification Manager
-  notMgr.begin (comm);
+	// Init Notification Manager
+	notMgr.begin (comm);
 
-  // Register Relay Controller (Notification Processor)
-  SensorProxy* ot = client.getSensor ("TK");
-  if (ot != NULL) {
-    rc.begin (relay, *ot);
-    notMgr.registerReceiver (rc, *ot, NT_PRD, 10);
-    //~ notMgr.registerReceiver (rc, *ot, NT_CHA);
-  }
+	// Register Relay Controller (Notification Processor)
+	SensorProxy* ot = client.getSensor ("TK");
+	if (ot != NULL) {
+		rc.begin (relay, *ot);
+		notMgr.registerReceiver (rc, *ot, NT_PRD, 10);
+		//~ notMgr.registerReceiver (rc, *ot, NT_CHA);
+	}
 
-  // Signal we're ready!
-  Serial.println (F("GO!"));
-  pinMode (LED_PIN, OUTPUT);
-  for (int i = 0; i < 3; i++) {
-    digitalWrite (LED_PIN, HIGH);
-    delay (100);
-    digitalWrite (LED_PIN, LOW);
-    delay (100);
-  }
+	// Signal we're ready!
+	Serial.println (F("GO!"));
+	pinMode (LED_PIN, OUTPUT);
+	for (int i = 0; i < 3; i++) {
+		digitalWrite (LED_PIN, HIGH);
+		delay (100);
+		digitalWrite (LED_PIN, LOW);
+		delay (100);
+	}
 }
 
 void loop (void) {
-  srv.loop ();
-  notMgr.loop ();
+	srv.loop ();
+	notMgr.loop ();
 }
