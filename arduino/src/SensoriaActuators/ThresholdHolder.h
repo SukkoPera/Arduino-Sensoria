bab1f9a322d3c24abd7b38122b16dd9863782e8b
SukkoPera <software@sukkology.net>
2018-05-12 09:42:30 +0200

    Add ThresholdHolder actuator

---
+++ arduino/src/SensoriaActuators/ThresholdHolder.h
@@ -0,0 +1,46 @@
+#ifndef THRESHOLDER_H_INCLUDED
+#define THRESHOLDER_H_INCLUDED
+
+#include <SensoriaCore/Actuator.h>
+#include <SensoriaStereotypes/ValueSetData.h>
+
+template <byte N>
+class ThresholdHolder: public Actuator<ValueSetData> {
+public:
+	static const byte N_TEMPS = N;
+
+	signed int thresholds[N_TEMPS];
+
+	boolean begin (FlashString name, FlashString description) {
+		return Actuator::begin (name, F("VS"), description, F("20180511"));
+	}
+
+	boolean write (ValueSetData& vs) override {
+		boolean ret = true;
+
+		for (byte i = 0; i < min (vs.nData, N_TEMPS); ++i) {
+			int tmp;
+			if (!vs.getDataInt (i, tmp)) {
+				DPRINT (F("Invalid threshold for V"));
+				DPRINT (i + 1);
+				DPRINT (F(": "));
+				DPRINTLN (vs.data[i]);
+
+				ret = false;
+			} else {
+				thresholds[i] = tmp;
+			}
+		}
+
+		return ret;
+	}
+
+	//~ boolean read (ValueSetData& vs) override {
+		//~ for (byte i = 0; i < min (vs.nData, N_TEMPS); ++i) {
+			//~ itoa (thresholds[i], vs.data[i], 10);
+		//~ }
+		//~ return true;
+	//~ }
+};
+
+#endif
