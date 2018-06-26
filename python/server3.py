d4aa912bccd2eeb8e1e6966c905d20ee732b6a40
SukkoPera <software@sukkology.net>
2018-06-27 01:30:15 +0200

    Remove Sensoria system services

--- python/server3.py
+++ python/server3.py
@@ -37,53 +37,15 @@ class HeaterSettings (server.ValueSetActuator):
 	def values (self, v):
 		self.levels = v[0:3]
 
-################################################################################
-
-
-# Import smtplib for the actual sending function
-import smtplib
-
-# Import the email modules we'll need
-from email.mime.text import MIMEText
-
-class EMailNotifier (server.Actuator):
-    def __init__ (self, name, description = "", sender = "sensoria@localhost", server = "localhost"):
-        super (EMailNotifier, self).__init__ (name, InstantMessageData.getIdString (), description)
-        self.sender = sender
-        self.server = server
-
-    def write (self, data):
-        assert isinstance (data, InstantMessageData)
-        msg = MIMEText (data.body)
-        msg['To'] = data.recipient
-        if data.subject is not None:
-            msg['Subject'] = data.subject
-        msg['From'] = self.sender
-
-        # Send the message via our own SMTP server, but don't include the
-        # envelope header.
-        s = smtplib.SMTP (self.server)
-        s.sendmail (self.sender, [data.recipient], msg.as_string ())
-        s.quit ()
-
-        return True, "Notification Sent"
-
-    # Read is not supported
-
-
 hd = TemperatureSensor ()
 hc = HeaterController ()
 ht = HeaterTimer ()
 hs = HeaterSettings ()
-ck = server.Clock ("$T", "System Clock")
-nt = EMailNotifier ("$N", "E-Mail Notifier")
 listener = server.CommandListener ("HeatingSystem")
 listener.register_sensor (hd)
 listener.register_sensor (hc)
 listener.register_sensor (ht)
 listener.register_sensor (hs)
-listener.register_sensor (ck)
-listener.register_sensor (nt)
 listener.start ()
 
 while True:
